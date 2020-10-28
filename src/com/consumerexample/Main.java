0f120aee957e91d32fad04290227dcaadd66d9cc
favorskij <favorskij@mail.ru>
2020-10-28 11:45:32 +1100

    Initial commit

---
+++ src/com/consumerexample/Main.java
@@ -0,0 +1,18 @@
+package com.consumerexample;
+
+import java.util.function.Consumer;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Consumer<Long> consumer1 = x -> System.out.println(x * 2);
+        consumer1.accept(2L);
+
+
+        Consumer<Long> consumer2 = System.out::println;
+        consumer2.accept(2L);
+
+    }
+}
